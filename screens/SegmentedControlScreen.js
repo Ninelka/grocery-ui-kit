ffc798178b2e390faf88312b0c44dfb5bd3ed8d1
Ninel <ninelgruzdeva@gmail.com>
2023-01-04 02:06:35 +0400

    feat: show SegmentedControl on SegmentedControlScreen

---
+++ screens/SegmentedControlScreen.js
@@ -0,0 +1,60 @@
+import SegmentedControl from "../components/SegmentedControl";
+import {useState} from "react";
+import {SafeAreaView, StyleSheet, Text, View} from "react-native";
+import {GlobalStyles} from "../constants/styles";
+
+const segmentsData = [
+    "Browse",
+    "Sale & Offer",
+    "Man",
+    "Woman",
+]
+
+function SegmentedControlScreen() {
+    const [activeSegment, setActiveSegment] = useState(segmentsData[0]);
+
+    return (
+        <SafeAreaView style={styles.container}>
+            <View style={styles.item}>
+                <View>
+                    <Text style={styles.label}>Transparent: false</Text>
+                </View>
+                <SegmentedControl
+                    segments={segmentsData}
+                    activeItem={activeSegment}
+                    onPress={(item) => setActiveSegment(item)}
+                />
+            </View>
+            <View style={styles.item}>
+                <View>
+                    <Text style={styles.label}>Transparent: true</Text>
+                </View>
+                <SegmentedControl
+                    transparent={true}
+                    segments={segmentsData}
+                    activeItem={activeSegment}
+                    onPress={(item) => setActiveSegment(item)}
+                />
+            </View>
+        </SafeAreaView>
+    )
+}
+
+export default SegmentedControlScreen;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    item: {
+        paddingVertical: GlobalStyles.spacing.s,
+        paddingHorizontal: GlobalStyles.spacing.m,
+        marginVertical: GlobalStyles.spacing.xs,
+        backgroundColor: GlobalStyles.colors.bgPrimary,
+        borderRadius: 16,
+        margin: GlobalStyles.spacing.xs
+    },
+    label: {
+        marginBottom: GlobalStyles.spacing.xs
+    },
+})
