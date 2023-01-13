ae8ee26405eac587b891cf783ce697671359585f
Ninel <ninelgruzdeva@gmail.com>
2023-01-13 15:42:38 +0500

    refactor: separate constants

---
+++ constants/fonts.ts
@@ -0,0 +1,8 @@
+export const FONT_FAMILY = {
+    regular: 'nunito-regular',
+    semiBold: 'nunito-semibold',
+    bold: 'nunito-bold',
+    regularItalic: 'nunito-italic',
+    semiBoldItalic: 'nunito-semibold-italic',
+    boldItalic: 'nunito-bold-italic',
+};
---
+++ constants/styles.ts
@@ -0,0 +1,28 @@
+export const GlobalStyles = {
+    fontSize: {
+        hugeTitle: 60,
+        largeTitle: 36,
+        title1: 24,
+        title2: 22,
+        title3: 20,
+        headline: 17,
+        callout: 16,
+        subhead: 14,
+        footnote: 12,
+        button: 16,
+        label1: 10,
+        label2: 20,
+        caption1: 12,
+        caption2: 10,
+    },
+    spacing: {
+        xs: 8,
+        s: 16,
+        m: 24,
+        l: 32,
+        xl: 40,
+        xxl: 48,
+        xxxl: 56,
+        xxxxl: 64
+    }
+};
